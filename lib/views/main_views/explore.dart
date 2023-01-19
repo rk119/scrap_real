3a4fe4971e8d19768bf2975c49a079591ecd58aa
Riffat <rk119@hw.ac.uk>
2023-01-19 13:52:31 +0400

    Implement explore page

--- lib/views/main_views/explore.dart
+++ lib/views/main_views/explore.dart
@@ -1,6 +1,12 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+
+import '../../themes/theme_provider.dart';
 
 class ExplorePage extends StatefulWidget {
   const ExplorePage({Key? key}) : super(key: key);
@@ -10,15 +16,68 @@ class ExplorePage extends StatefulWidget {
 }
 
 class _ExplorePageState extends State<ExplorePage> {
+  Completer<GoogleMapController> _controller = Completer();
+  LatLng? _currentPosition;
+  BitmapDescriptor currentPositionIcon = BitmapDescriptor.defaultMarker;
+
+  setCustomMarkerIcon() async {
+    currentPositionIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(), "assets/markers/current_location.png");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    setCustomMarkerIcon();
+  }
+
+  getLocation() async {
+    await Geolocator.requestPermission();
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    double lat = position.latitude;
+    double long = position.longitude;
+
+    LatLng location = LatLng(lat, long);
+
+    setState(() {
+      _currentPosition = location;
+    });
+
+    print(_currentPosition);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Explore",
-          style: TextStyle(fontSize: 28),
-        ),
-      ),
+      body: _currentPosition == null
+          ? Container(
+              color: Provider.of<ThemeProvider>(context).themeMode ==
+                      ThemeMode.dark
+                  ? Colors.grey.shade900
+                  : Colors.white,
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: CameraPosition(
+                target: _currentPosition!,
+                zoom: 14,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              markers: {
+                  Marker(
+                    markerId: const MarkerId('currentLocation'),
+                    position: _currentPosition!,
+                    icon: currentPositionIcon,
+                  ),
+                }),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,8 @@ import firebase_analytics
 import firebase_auth
 import firebase_core
 import firebase_storage
+import geolocator_apple
+import location
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
