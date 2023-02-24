62cfed53b2d5d5d049ba7486a00773ca8bb8f173
Riffat <rk119@hw.ac.uk>
2023-02-24 13:21:21 +0400

    Made responsive

--- lib/views/auth_views/welcome.dart
+++ lib/views/auth_views/welcome.dart
@@ -23,104 +23,111 @@ class _WelcomePageState extends State<WelcomePage> {
       body: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          return Container(
-            padding: const EdgeInsets.fromLTRB(30, 120, 30, 0),
-            width: double.infinity,
-            // decoration: const BoxDecoration(
-            //   color: Color(0xffffffff),
-            // ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                DropShadow(
-                  blurRadius: 3,
-                  offset: const Offset(4, 4),
-                  child: SvgPicture.asset(
-                    'assets/logo.svg',
-                  ),
-                ),
-                DropShadow(
-                  blurRadius: 2,
-                  offset: const Offset(0, 2),
-                  child: SvgPicture.asset(
-                    'assets/pattern.svg',
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 30, 0, 30),
-                  child: RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      style: GoogleFonts.jua(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w400,
-                        height: 1.25,
-                        color: Provider.of<ThemeProvider>(context).themeMode ==
-                                ThemeMode.dark
-                            ? Colors.white
-                            : Colors.black,
-                      ),
-                      children: [
-                        const TextSpan(
-                          text: 'Welcome to ',
+          return Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+              Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      DropShadow(
+                        blurRadius: 3,
+                        offset: const Offset(4, 4),
+                        child: SvgPicture.asset(
+                          'assets/logo.svg',
                         ),
-                        TextSpan(
-                          text: 'ScrapReal',
+                      ),
+                      DropShadow(
+                        blurRadius: 2,
+                        offset: const Offset(0, 2),
+                        child: SvgPicture.asset(
+                          'assets/pattern.svg',
+                        ),
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.04),
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
                           style: GoogleFonts.jua(
                             fontSize: 30,
                             fontWeight: FontWeight.w400,
                             height: 1.25,
-                            color: const Color(0xff918ef4),
+                            color:
+                                Provider.of<ThemeProvider>(context).themeMode ==
+                                        ThemeMode.dark
+                                    ? Colors.white
+                                    : Colors.black,
+                          ),
+                          children: [
+                            const TextSpan(
+                              text: 'Welcome to ',
+                            ),
+                            TextSpan(
+                              text: 'ScrapReal',
+                              style: GoogleFonts.jua(
+                                fontSize: 30,
+                                fontWeight: FontWeight.w400,
+                                height: 1.25,
+                                color: const Color(0xff918ef4),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.03),
+                      Container(
+                        constraints: BoxConstraints(
+                          maxWidth: MediaQuery.of(context).size.width * 0.8,
+                        ),
+                        child: Text(
+                          'Make memories and revist them.\nAnytime. Any place.',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                            height: 1.5,
+                            color: const Color(0xffa09f9f),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.05),
+                      CustomTextButton(
+                        key: const Key('registerPage'),
+                        buttonBorderRadius: BorderRadius.circular(10),
+                        buttonFunction: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterPage()),
+                          );
+                        },
+                        buttonText: "Get Started",
+                        buttonColor: const Color(0xfff8f5f5),
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.03),
+                      CustomTextButton(
+                        key: const Key('loginPage'),
+                        buttonBorderRadius: BorderRadius.circular(10),
+                        buttonFunction: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginPage()),
+                          );
+                        },
+                        buttonText: "I already have an account",
+                      ),
+                    ],
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(4, 0, 0, 40),
-                  constraints: const BoxConstraints(
-                    maxWidth: 295,
-                  ),
-                  child: Text(
-                    'Make memories and revist them.\nAnytime. Any place.',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                      height: 1.5,
-                      color: const Color(0xffa09f9f),
-                    ),
-                  ),
-                ),
-                CustomTextButton(
-                  key: const Key('registerPage'),
-                  buttonBorderRadius: BorderRadius.circular(10),
-                  buttonFunction: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const RegisterPage()),
-                    );
-                  },
-                  buttonText: "Get Started",
-                  buttonColor: const Color(0xfff8f5f5),
-                ),
-                const SizedBox(height: 23),
-                CustomTextButton(
-                  key: const Key('loginPage'),
-                  buttonBorderRadius: BorderRadius.circular(10),
-                  buttonFunction: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const LoginPage()),
-                    );
-                  },
-                  buttonText: "I already have an account",
-                ),
-              ],
-            ),
+              ),
+            ],
           );
         },
       ),
